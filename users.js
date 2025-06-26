ccd113274c08b229b95d6c1a3e8a6dc2c2cf8698
SK-SHUVOJIT <98234575+SHUVO-46@users.noreply.github.com>
2025-06-27 02:11:58 +0600

    Add files via upload

---
+++ users.js
@@ -0,0 +1,20 @@
+const users = [
+    { username: 'Monirul', password: 'Monirul@1', name: 'Monirul Islam' },
+    { username: 'prantomondal878', password: 'Pranto123', name: 'Pranto Mondal' },
+    { username: 'badhon29', password: 'bs2949', name: 'Badhon Das' },
+    { username: 'partho0000', password: 'Partho000', name: 'Partho Sarkar' },
+    { username: 'tufan', password: 'admin07', name: 'TUFAN (SUB-ADMIN)' },
+    { username: 'Nitish', password: '347685', name: 'Nitish Kumar' },
+    { username: 'Bappy0203', password: 'Bappy2003', name: 'Ariyan Bappy' },
+    { username: 'ADMIN8', password: 'ADMIN8', name: 'Somiron Mondal' },
+    { username: 'rana120', password: '@@rana12', name: 'Rana Vai' },
+    { username: 'Dweep', password: '501135', name: 'Dweep' },
+    { username: 'Sunna@123', password: 'Sunna@2006', name: 'Amdad Ahmed' },
+    { username: 'Nirob', password: 'Nirob@46', name: 'Nirob Raj' },
+    { username: '@apurbo2122', password: 'Apurbo2122', name: 'Apurbo Sarkar' },
+    { username: '@avi4321', password: 'Avijit4321', name: 'Avijit Mondal' },
+];
+
+const okUIDs = [
+    "111111", "222222", "333333" // এখানে তোমার OK UID গুলো বসাও
+];
